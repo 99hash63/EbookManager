b0593c4e0c27cfad518679110c4c694763ec8ce3
Hashen Kottegoda <thkottegoda@gmail.com>
2024-06-30 14:53:59 +0530

    initialized project using spring initializer

---
+++ ebook-manager-api/src/test/java/com/hashen/ebook_manager/EbookManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashen.ebook_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
